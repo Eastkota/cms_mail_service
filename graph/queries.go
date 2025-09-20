3f9521753d324d35eb234281925124705282fffc
pelzang <pelzangtp@gmail.com>
2025-09-20 16:54:15 +0600

    updated

--- graph/queries.go
+++ graph/queries.go
@@ -21,7 +21,7 @@ func NewQueryType(resolver *resolvers.MailResolver) *graphql.Object {
                         return nil, err
                     }
                     serviceInfo := model.Service{
-                        Name:    "mailService",
+                        Name:    "MailService",
                         Version: "1.0.0",
                         Schema:  helpers.ConvertSchemaToString(schema),
                     }
