204cd621dafc89dca51fab11b2cdcaa3496fd68e
suraceallay <suraceallay@gmail.com>
2025-09-26 17:05:26 +0600

    commit brfore pull

--- resolvers/mail_resolver.go
+++ resolvers/mail_resolver.go
@@ -4,11 +4,6 @@ import (
 	"cms_mail_service/helpers"
 	"cms_mail_service/model"
 	"cms_mail_service/services"
-<<<<<<< Updated upstream
-=======
-	"encoding/json"
-	"fmt"
->>>>>>> Stashed changes
 
 	"github.com/graphql-go/graphql"
 )
@@ -33,31 +28,3 @@ func (r *MailResolver) FetchEmailLog(p graphql.ResolveParams) *model.EmailLogRes
         Error: nil,
     }
 }
-<<<<<<< Updated upstream
-=======
-
-func (r *MailResolver) SendMail(p graphql.ResolveParams) *model.GenericMailResponse {
-	var mailInput model.MailInput
-	inputData := p.Args["input"].(map[string]interface{})
-	jsonData, err := json.Marshal(inputData)
-	if err != nil {
-		return helpers.FormatError(err)
-	}
-	err = json.Unmarshal(jsonData, &mailInput)
-	if err != nil {
-		return helpers.FormatError(err)
-	}
-
-	err = r.Services.SendMail(mailInput)
-	if err != nil {
-		return helpers.FormatError(err)
-	}
-    fmt.Println(mailInput)
-	return &model.GenericMailResponse{
-		Data: &model.GenericMailSuccessData{
-			Message: "Email sent succesfully",
-		},
-		Error: nil,
-	}
-}
->>>>>>> Stashed changes
